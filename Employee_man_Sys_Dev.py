6734fdcd27b24dd68d8123e314703f870628a72a
jeissonco <145677040+jeissonco@users.noreply.github.com>
2025-05-26 22:52:22 +0800

    first commit

---
+++ Employee_man_Sys_Dev.py
@@ -0,0 +1,33 @@
+# @Author: Jeisson Nino
+# date: 26/05/2025
+
+
+
+######################  Libraries  #######################
+import os
+import csv
+####################  functions ##########################
+
+
+###################  Classes  ############################
+
+class Employee:
+    def __init__(self, name, age, position, salary, department, location):
+        #private attributes
+        self.__name = name
+        self.__age = age
+        self.__position = position
+        self.__salary = salary
+
+        #public attributes
+        self.department = department
+        self.location = location
+        
+    
+
+
+
+
+
+####################  Main  ############################### 
+
